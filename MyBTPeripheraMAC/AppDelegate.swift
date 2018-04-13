ab50c64fcd19974cc11d7bd6b3a72803df746f1a
desktop <desktop@desktop.com>
2018-04-13 15:11:06 +0800

    Initial Commit

---
+++ MyBTPeripheraMAC/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyBTPeripheraMAC
+//
+//  Created by KaiChieh on 2018/4/13.
+//  Copyright © 2018 KaiChieh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyBTPeripheraMAC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyBTPeripheraMAC
+//
+//  Created by KaiChieh on 2018/4/13.
+//  Copyright © 2018 KaiChieh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
